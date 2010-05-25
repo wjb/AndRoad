87bef8d50f263da60782b35aa345688e80469dbb
valentin.blot0 <valentin.blot0@a796ab36-f541-11de-b359-dd99991af1b2>
2010-05-25 00:03:15 +0000

    nexus one bugfix
    git-svn-id: http://andnav.googlecode.com/svn/trunk@47 a796ab36-f541-11de-b359-dd99991af1b2

--- src/org/andnav2/osm/views/util/constants/OSMMapViewConstants.java
+++ src/org/andnav2/osm/views/util/constants/OSMMapViewConstants.java
@@ -13,7 +13,7 @@ public interface OSMMapViewConstants {
 
 	public static final boolean DEBUGMODE = false;
 
-	public static final int CACHE_MAPTILECOUNT_DEFAULT = 20;
+	public static final int CACHE_MAPTILECOUNT_DEFAULT = 40;
 
 	public static final String PREF_MAPCENTER_ID = "pref_mapcenter_id";
 
